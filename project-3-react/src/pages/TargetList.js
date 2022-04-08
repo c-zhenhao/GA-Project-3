cdee7519abcb4d8285242659910c3536e94f0ef5
yixinlee <yixin.lee.2008@gmail.com>
2022-04-08 11:07:49 +0800

    template with targetlist done with mock seed data

--- project-3-react/src/pages/TargetList.js
+++ project-3-react/src/pages/TargetList.js
@@ -1,14 +1,21 @@
 import React from "react";
-const seed = require ("../../project-3-express/models/seed");
+import { Link } from "react-router-dom"; 
+const seed = require ("../components/modals/seed");
 
 const TargetList = () => {
+
   const seedList = seed.map((data, i)=> {
+    const userId = "zh";
+    const targetId = "aye";
+
     return (
       <li key={i}>
         {data.displayName}
         {data.userRating}
         <img src={data.imgUrl}/>
+        <Link to={`/${userId}/profile/${targetId}`}> See Profile </Link>
       </li>
+
       
     )
   })
