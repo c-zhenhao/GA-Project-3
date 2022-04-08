001f9c0de9b9a4ba18ff0057a6678bef14b010e0
yixinlee <yixin.lee.2008@gmail.com>
2022-04-08 10:25:42 +0800

    Update TargetList.js

--- project-3-react/src/pages/TargetList.js
+++ project-3-react/src/pages/TargetList.js
@@ -1,7 +1,24 @@
 import React from "react";
+const seed = require ("../../project-3-express/models/seed");
 
 const TargetList = () => {
-  return <div>This is Target List View.</div>;
+  const seedList = seed.map((data, i)=> {
+    return (
+      <li key={i}>
+        {data.displayName}
+        {data.userRating}
+        <img src={data.imgUrl}/>
+      </li>
+      
+    )
+  })
+  return (
+    <div>
+    <h1>This is Target List View.</h1>
+    {seedList} 
+    </div>
+  )
+  
 };
 
 export default TargetList;
