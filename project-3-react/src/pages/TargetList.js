d494c44211d1c7cdde70a6d071133379400f3f73
yixinlee <yixin.lee.2008@gmail.com>
2022-04-11 14:41:51 +0800

    updated for profile and targetlist with api

--- project-3-react/src/pages/TargetList.js
+++ project-3-react/src/pages/TargetList.js
@@ -4,28 +4,27 @@ import Rating from '@mui/material/Rating';
 import Typography from '@mui/material/Typography';
 import { Link } from "react-router-dom"; 
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
-//const seed = require ("../components/modals/seed");
+const matchURL = `${process.env.REACT_APP_SERVER_DOMAIN}/seed`;
 
 const TargetList = () => {
   //using API to populate seed
-  const [list, setList] = useState(null);
+  const [list, setList] = useState([]);
 
-  useEffect (()=> {
-    fetch('http://localhost:3000/seed')//need to replace with :id right?
-    .then(res => {
-      return res.json();
-    })
-    .then(data => {
-      setList(data);
-    }) 
-  }, []);
+  useEffect (async ()=> {
+  try {
+    const response  = await axios.get(matchURL);
+  setList(response.data);
+} catch (error) {
+  console.log(error)
+}
+}, []);
   
   const params = useParams();
-  const displayList = list.map((data, i)=> {
-    const userId = params.id;
-    const targetId = params.target;
+  const userId = params.id;
 
+  const displayList = list.map((data, i)=> {
     return (
       <ul key={i}>
         <b>{data.displayName}</b>
@@ -38,15 +37,13 @@ const TargetList = () => {
       <Rating name="read-only" value={data.userRating} precision={0.5} size="large" readOnly />
     </Box>
         <img src={data.imgUrl}/>
-       <p><Link to={`/${userId}/profile/${targetId}`}> See Profile </Link></p>
-      </ul>
-
-      
+       <p><Link to={`/${userId}/profile/${data._id}`}> See Profile </Link></p>
+      </ul>      
     )
   })
   return (
     <div>
-    <h1>This is Target List View.</h1>
+    <h1>Your matches</h1>
     {displayList} 
     </div>
   )
