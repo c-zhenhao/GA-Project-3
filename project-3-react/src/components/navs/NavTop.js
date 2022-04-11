f0e0096f03d030cabdcde474f7d8519f6f3575c9
czhenhao <czhenhao@hotmail.com>
2022-04-11 15:30:07 +0800

    updated navbar with logout

--- project-3-react/src/components/navs/NavTop.js
+++ project-3-react/src/components/navs/NavTop.js
@@ -1,28 +1,36 @@
 import * as React from "react";
+import { Link as RouterLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import MenuIcon from "@mui/icons-material/Menu";
 import Badge from "@mui/material/Badge";
 import NotificationsIcon from "@mui/icons-material/Notifications";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 export default function NavTop() {
+  const userId = useSelector((state) => state.user.userId);
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
+          {userId && (
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="logout"
+              component={RouterLink}
+              to={`/logout`}
+            >
+              <LogoutIcon />
+            </IconButton>
+          )}
+
           <Typography
             variant="h6"
             noWrap
@@ -32,15 +40,17 @@ export default function NavTop() {
             OnlyFriends
           </Typography>
 
-          <IconButton
-            size="large"
-            aria-label="show 17 new notifications"
-            color="inherit"
-          >
-            <Badge badgeContent={69} color="error">
-              <NotificationsIcon />
-            </Badge>
-          </IconButton>
+          {userId && (
+            <IconButton
+              size="large"
+              aria-label="show 17 new notifications"
+              color="inherit"
+            >
+              <Badge badgeContent={69} color="error">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
