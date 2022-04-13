268094a8b69897362707fbf7461e184fbbea03ae
blueberry <kuangyongchang@gmail.com>
2022-04-13 11:04:04 +0800

    fixed list userRating error

--- project-3-express/controllers/match.js
+++ project-3-express/controllers/match.js
@@ -11,25 +11,20 @@ const dbError = {
 
 router.get("/", auth, async (req, res) => {
   try {
-    const user = await Users.findById(req.session.userId);
-    console.log(user);
-    const filter = user.userInteracted.map((target) => target.targetUsername);
+    const { userInteracted } = await Users.findById(req.session.userId);
+    const filter = userInteracted.map(({ targetUsername }) => targetUsername);
     filter.push(req.session.currentUser);
-    console.log(filter);
     res.json(await Users.find({ username: { $nin: filter } }, { passwordHash: 0 }));
   } catch (err) {
-    console.error(err);
+    // console.error(err);
     res.status(400).json(dbError);
   }
 });
 
 router.post("/", auth, async (req, res) => {
   try {
-    const user = await Users.findById(req.session.userId, { _id: 0, userInteracted: 1 });
-    const userInteracted = user.userInteracted;
-    // req.body.targetRating = null;
+    const { userInteracted } = await Users.findById(req.session.userId);
     userInteracted.push(req.body);
-    // console.log(userInteracted);
     await Users.findByIdAndUpdate(req.session.userId, { userInteracted });
 
     if (req.body.swiped) {
@@ -37,12 +32,22 @@ router.post("/", auth, async (req, res) => {
       const targetInteracted = target.userInteracted.filter(
         (target) => target.targetUsername === req.session.currentUser && target.swiped
       );
-      console.log(targetInteracted);
       if (targetInteracted.length === 1) res.json({ matched: true });
       else res.json({ matched: false });
     } else {
       res.json({ title: "OK", message: "swiped successful" });
     }
+  } catch (err) {
+    // console.error(err);
+    res.status(400).json(dbError);
+  }
+});
+
+router.patch("/filters", auth, async (req, res) => {
+  try {
+    const { userPreference } = req.body;
+    await Users.findByIdAndUpdate(req.session.userId, { userPreference });
+    res.json({ title: "OK", message: "filters updated" });
   } catch (err) {
     console.error(err);
     res.status(400).json(dbError);
